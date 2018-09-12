f8c6122406b7a09691e14832afd4e30baa6f804d
Natan <natancunha@gmail.com>
2018-09-12 20:47:52 -0300

    Pagina novidades e alteração do botão

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { NovidadePage } from '../novidade/novidade';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoNovidade(){
+    this.navCtrl.push(NovidadePage);
   }
 
 }
